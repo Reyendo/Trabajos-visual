8d9a17c96716a8ee45b49b2261a5dc2854359880
Reyendo <elian.andres050971@gmail.com>
2025-08-20 09:47:55 -0600

    Add files via upload

---
+++ ejer1/Form1.Designer.cs
@@ -0,0 +1,113 @@
+﻿namespace ejer1
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.User = new System.Windows.Forms.TextBox();
+            this.pass = new System.Windows.Forms.TextBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(38, 43);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(149, 25);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Inserte Usuario:";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.Location = new System.Drawing.Point(38, 95);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(184, 25);
+            this.label2.TabIndex = 1;
+            this.label2.Text = "Inserte Contrasena:";
+            // 
+            // User
+            // 
+            this.User.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.User.Location = new System.Drawing.Point(208, 43);
+            this.User.Name = "User";
+            this.User.Size = new System.Drawing.Size(422, 30);
+            this.User.TabIndex = 2;
+            this.User.TextChanged += new System.EventHandler(this.textBox1_TextChanged);
+            // 
+            // pass
+            // 
+            this.pass.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.pass.Location = new System.Drawing.Point(243, 95);
+            this.pass.Name = "pass";
+            this.pass.PasswordChar = '*';
+            this.pass.Size = new System.Drawing.Size(387, 30);
+            this.pass.TabIndex = 3;
+            this.pass.TextChanged += new System.EventHandler(this.textBox2_TextChanged);
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(228, 171);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(139, 42);
+            this.button1.TabIndex = 4;
+            this.button1.Text = "INICIAR SESION";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(687, 241);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.pass);
+            this.Controls.Add(this.User);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox User;
+        private System.Windows.Forms.TextBox pass;
+        private System.Windows.Forms.Button button1;
+    }
+}
+
---
+++ ejer1/Form1.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace ejer1
+{
+    public partial class Form1 : Form
+    {
+        int z= 0;
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void textBox1_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            string a = User.Text;
+            string b = pass.Text;
+            if(a=="" || b == "")
+            {
+                MessageBox.Show("Complete los campos","Error",MessageBoxButtons.RetryCancel,MessageBoxIcon.Error);
+            }
+            else
+            {
+                if (a == "admin" && b == "admin123")
+                {
+                    MessageBox.Show("Bienvenido Admin", "Bienvenido", MessageBoxButtons.OK, MessageBoxIcon.None);
+                    User.Text = "";
+                    pass.Text = "";
+                }
+                else
+                {
+                    MessageBox.Show("Usuario o Contrasena incorrectos, le quedan "+ (3-z) +" intentos.", "Incorrecto", MessageBoxButtons.OK, MessageBoxIcon.Exclamation);
+                    z = z + 1;
+                    if (z == 4)
+                    {
+                        MessageBox.Show("Se ha quedado sin intentos", "Sin intentos", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                        Close();
+                    }
+                }
+            }
+        }
+        
+
+        private void textBox2_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
